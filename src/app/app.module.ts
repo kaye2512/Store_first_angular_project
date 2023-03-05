a3667d8311dd1323a4d67f3b046674260125a8e3
kaye romuald <kayeromy@gmail.com>
2023-03-06 00:45:12 +0100

    setup angular projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSidenav, MatSidenavModule} from "@angular/material/sidenav";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatExpansionModule} from "@angular/material/expansion";
+import {MatListModule} from "@angular/material/list";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTableModule} from "@angular/material/table";
+import {MatBadgeModule} from "@angular/material/badge";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
@@ -10,7 +22,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+      MatSidenavModule,
+      MatGridListModule,
+      MatMenuModule,
+      MatButtonModule,
+      MatCardModule,
+      MatExpansionModule,
+      MatListModule,
+      MatToolbarModule,
+      MatTableModule,
+      MatBadgeModule,
+      MatSnackBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
