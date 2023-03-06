661e65ad8ccd14f1ed39cf595f4b0b2aba211c6b
kaye romuald <kayeromy@gmail.com>
2023-03-06 02:14:45 +0100

    navbar

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  // selector est appelé dans app.components
+  selector: 'app-header',
+  // Call html file
+  templateUrl: `header.component.html`,
+})
+export class HeaderComponent {
+
+}
