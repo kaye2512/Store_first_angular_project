8d40565cb14f10937c4cd136de65fc57e51e9d6d
kaye romuald <kayeromy@gmail.com>
2023-03-08 11:10:22 +0100

    product view

--- src/app/pages/home/components/product-header/product-header.component.ts
+++ src/app/pages/home/components/product-header/product-header.component.ts
@@ -1,9 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-product-header',
   templateUrl: `product-header.component.html`,
 })
 export class ProductHeaderComponent {
+  @Output() columnsCountChange = new EventEmitter<number>();
+  //on passe la valeur desc par defaut
+  sort = 'desc';
+  itemsShowCount = 12;
 
+  // on crée une methode pour update la valeur desc lorsqu'on clic sur une valeur
+  onSortUpdated(newSort: string): void {
+      this.sort = newSort;
+  }
+
+  onItemsUpdated(count: number): void {
+    this.itemsShowCount = count;
+  }
+
+  onColumnsUpdated(colsNum: number): void {
+    this.columnsCountChange.emit(colsNum);
+  }
 }
